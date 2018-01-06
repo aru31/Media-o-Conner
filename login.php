b23b220aa3ee85579a2fd6a21cc8a1b3b67e5553
Arpit Gupta <guptarpit1997@gmail.com>
2018-01-06 14:47:50 +0530

    adding login page

---
+++ login.php
@@ -0,0 +1,50 @@
+<?php
+ require('server.php'); 
+
+ ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>login</title>
+	<link rel="stylesheet" type="text/css" href="project.css">
+ 
+</head>
+<body>
+	<div class="login_page">
+  		
+    		<form  action="login.php" method="POST">
+          <?php require('errors.php'); ?>
+
+<table>
+
+      		 <tr>
+            <th><label for="username">Name</label></th>
+            <td><input type="text" id="username" name="username" /></td>
+      </tr>
+        
+
+         <tr>
+            <th><label for="password">Password</label></th>
+            <td><input type="password" id="password" name="password" /></td>
+      </tr>
+
+       <tr style="display:none">
+            <th><label for="address">Address</label></th>
+            <td><input type="text" id="address" name="address" /><p>Please leave this field blank</p></td>
+      </tr>
+      
+</table>  
+      		 <input type="submit" value="Login" name="log_page" />
+
+      	<p class="login_">Not registered? <a href="register.php">Create an account</a></p>
+  	
+      	</form>
+  
+</div>
+
+</body>
+</html>
+
+
+
